21928d4552b7fb992ea7ec3373771d92c19f4d3d
Cesar Castro <ceh.castro99@gmail.com>
2025-05-14 19:19:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: Hydration failed because the server rendered HTML didn't match the client. As a result this tree will be regenerated on the client. This can happen if a SSR-ed Client Component used:. Error source: - A server/client branch `if (typeof window !== 'undefined')`.
    > - Variable input such as `Date.now()` or `Math.random()` which changes each time it's called.
    > - Date formatting in a user's locale which doesn't match the server.
    > - External changing data without sending a snapshot of it along with the HTML.
    > - Invalid HTML tag nesting.
    >
    > It can also happen if the client has a browser extension installed which messes with the HTML before React loaded.
    >
    > See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    >   ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <RootLayout>
    >                       <html lang="en" className="dark">
    >                         <body
    >                           className="geist_e531dabc-module__QGiZLq__variable geist_mono_68a01160-module__YLcDdW__varia..."
    > -                         data-new-gr-c-s-check-loaded="14.1235.0"
    > -                         data-gr-ext-installed=""
    >                         >
    >                     ...
    >         ...
    >
    >
    > src/app/layout.tsx (29:7) @ RootLayout
    >
    >   27 |   return (
    >   28 |     <html lang="en" className="dark">
    > > 29 |       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
    >      |       ^
    >   30 |         <SidebarProvider defaultOpen={true}>
    >   31 |           {children}
    >   32 |         </SidebarProvider>
    >
    > Call Stack
    > 17
    >
    > Show 15 ignore-listed frame(s)
    > body
    > <anonymous> (0:0)
    > RootLayout
    > src/app/layout.tsx (29:7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body suppressHydrationWarning={true} className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <SidebarProvider defaultOpen={true}>
           {children}
         </SidebarProvider>
