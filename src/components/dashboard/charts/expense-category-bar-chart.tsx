9bdf797e50fc501359aaa191e57d78a25371e1ed
Cesar Castro <ceh.castro99@gmail.com>
2025-06-25 16:51:05 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.div dat

--- src/components/dashboard/charts/expense-category-bar-chart.tsx
+++ src/components/dashboard/charts/expense-category-bar-chart.tsx
@@ -83,8 +83,8 @@ export function ExpenseCategoryBarChart({ transactions, userCategories }: Expens
   }
 
   return (
-    <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
-      <ResponsiveContainer width="100%" height={Math.max(200, expenseData.length * 20)}>
+    <ChartContainer config={chartConfig} className="min-h-[150px] w-full">
+      <ResponsiveContainer width="100%" height={Math.max(150, expenseData.length * 18)}>
         <BarChart
           data={expenseData}
           layout="vertical"
@@ -118,7 +118,7 @@ export function ExpenseCategoryBarChart({ transactions, userCategories }: Expens
               />
             }
           />
-          <Bar dataKey="value" layout="vertical" radius={[0, 4, 4, 0]} barSize={12}>
+          <Bar dataKey="value" layout="vertical" radius={[0, 4, 4, 0]} barSize={10}>
             {expenseData.map((entry, index) => (
               <Cell
                 key={`cell-${index}`}
